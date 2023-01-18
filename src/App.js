e5b7428f535a867b44cba165764de4f1005b7c14
Crystal <crystal123@hotmail.com>
2023-01-18 21:31:29 +1300

    finished

--- src/App.js
+++ src/App.js
@@ -4,10 +4,46 @@ import JobBoardComponent from './components/JobBoardComponent';
 
 function App() {
   const [jobs, setJobs] = useState([]); 
-
+  const [filters, setFilters] = useState([]);
 
   //updates the array
   useEffect(() => setJobs(data), []);
+  
+    const filterFunc = ({role, level, tools, languages}
+      ) => {
+        if(filters.length === 0) {
+          return true;
+        }
+
+      const tags = [role, level];
+
+      if(tools) {
+        tags.push(...tools);
+    }
+
+    if(languages) {
+        tags.push(...languages);
+    }
+    
+    return tags.some((tag) => filters.includes(tag));
+    }
+
+
+const handleTagClick = (tag) => {
+  if(filters.includes(tag)) return;
+  setFilters([...filters, tag]);
+}
+
+const handleFilterClick = (passedFilter) => {
+  setFilters(filters.filter(f => f !== passedFilter))
+}
+
+
+  const filteredJobs = jobs.filter(filterFunc);
+
+  const clearFilters = () => {
+    setFilters([]);
+  }
 
 
   return (
@@ -15,12 +51,36 @@ function App() {
       <header>
         <img src='/images/bg-header-desktop.svg' alt='bg-image' />
       </header>
-          {/* <h1 className="text-4xl text-center my-6 font-bold">Job Listings</h1> */}
+
+      <div className='flex bg-white shadow-md my-16 mx-10 p-6 rounded'>
+        {
+          filters.length > 0 && filters.map(
+            (filter) => <span
+            className='text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0'
+            >{filter}
+            <span 
+            onClick={() =>
+            handleFilterClick(filter)}
+            className='cursor-pointer bg-teal-500 text-teal-100 px-2 py-1 ml-3'>X</span>
+            </span>
+          )}
+          <button
+          onClick={clearFilters}
+          className='font-bold text-gray-700 ml-auto'
+          >Clear</button>
+      </div>
+
       {jobs.length === 0 ? (
           <p>Jobs are fetching...</p>
         ) : (
-          jobs.map((job) => <JobBoardComponent job={job} key={job.id} />)
-          )}
+          filteredJobs.map((job) => (
+          <JobBoardComponent 
+          job={job} 
+          key={job.id} 
+          handleTagClick={handleTagClick}
+          />
+          ))
+        )}
     </div>
   );
 }
--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -4,7 +4,7 @@ const JobBoardComponent = ({
     job: {
         company,
         logo,
-        New,
+        isNew,
         featured,
         position,
         role,
@@ -15,34 +15,50 @@ const JobBoardComponent = ({
         languages,
         tools,
     },
+    handleTagClick,
 }) => {
-    const langAndTools = [];
+    const tags = [role, level];
 
     if(tools) {
-        langAndTools.push(...tools);
+        tags.push(...tools);
     }
 
     if(languages) {
-        langAndTools.push(...languages);
+        tags.push(...languages);
     }
 
+
     return (
-<div className="flex bg-white shadow-lg m-4 p-4">
+<div className={`flex bg-white shadow-lg m-4 p-6 rounded ${
+    featured && 'border-l-4 border-teal-500 border-solid'
+}`}>
 
     <div>
         <img className='px-8' src={logo} alt={company} />
     </div>
     <div className='flex flex-col justify-between ml-4'>
-        <h3 className='font-bold text-teal-500'>{company}</h3>
+        <h3 className='font-bold text-teal-500'>
+            {company}
+            {isNew && (
+            <span className='bg-teal-500 text-teal-100 font-bold m-2 px-2 py-1 rounded-full'>
+                New
+            </span>)}
+            {featured && (
+            <span className='bg-gray-800 text-white font-bold px-2 py-1 rounded-full'>
+                Featured
+            </span>)}
+            </h3>
         <h2 className='font-bold text-xl'>{position}</h2>
         <p className='text-gray-700'>
             {postedAt} · {contract} · {location}
         </p>
     </div>
     <div className='flex items-center ml-auto'>
-        {langAndTools ? langAndTools.map((langAndTool) => 
-        <span className='text-teal-500 bg-teal-100 font-bold m-3 p-2 rounded'>
-            {langAndTool}
+        {tags ? tags.map((tag) => 
+        <span
+        onClick={() => handleTagClick(tag)} 
+        className='text-teal-500 bg-teal-100 font-bold m-3 p-2 rounded'>
+            {tag}
             </span>) : ''}
     </div>
 
